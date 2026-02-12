021a03ac2a3510aa00be9d8ba8e53b9d404a7c04
ThorSonOfThors <filintros@gmail.com>
2026-02-12 15:14:48 +0700

    Ad verification
    Changed  nuxt.config.ts file so that it uses the google ads authetification.

--- tile-calculator/nuxt.config.ts
+++ tile-calculator/nuxt.config.ts
@@ -1,7 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  css: ['~/assets/css/base.css'],
-})
 
+  devtools: { enabled: true },
+
+  css: ['~/assets/css/base.css'],
+
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9341226393355638',
+          async: true,
+          crossorigin: 'anonymous'
+        }
+      ]
+    }
+  }
+})
